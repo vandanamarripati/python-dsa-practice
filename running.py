50c46dfe153011fa575a5106437db0a5bfec509f
vandanamarripati <vandanamarripati@gmail.com>
2026-05-11 16:21:44 +0530

    Added running sum solution

---
+++ running.py
@@ -0,0 +1,6 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        s=0
+        for i in range(1,len(nums)):
+            nums[i]=nums[i]+nums[i-1]
+        return nums
